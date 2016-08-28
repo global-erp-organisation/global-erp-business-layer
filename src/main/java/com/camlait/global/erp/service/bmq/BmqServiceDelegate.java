130f84f8c76686925102ddea241765f0ec173baa
Martin Mbossi <Martin Mbossi>
2016-08-28 11:02:57 -0400

    update the pom file and improve java 8 statements.

--- src/main/java/com/camlait/global/erp/service/bmq/BmqServiceDelegate.java
+++ src/main/java/com/camlait/global/erp/service/bmq/BmqServiceDelegate.java
@@ -3,8 +3,8 @@ package com.camlait.global.erp.service.bmq;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -21,145 +21,139 @@ import com.camlait.global.erp.domain.model.json.bmq.LigneBmqModel;
 import com.camlait.global.erp.domain.model.json.bmq.LigneBmqTaxeModel;
 import com.camlait.global.erp.domain.model.json.operation.RecouvrementModel;
 import com.camlait.global.erp.domain.operation.Recouvrement;
-
 public class BmqServiceDelegate {
 
-	@Autowired
-	private IBmqService bmqService;
+    @Autowired
+    private IBmqService bmqService;
 
-	public BmqModel ajouterBmq(Bmq bmq) {
-		return new BmqModel(bmqService.ajouterBmq(bmq));
-	}
+    public BmqModel ajouterBmq(Bmq bmq) {
+        return new BmqModel(bmqService.ajouterBmq(bmq));
+    }
 
-	public BmqModel modifierBmq(Bmq bmq) {
-		return new BmqModel(bmqService.modifierBmq(bmq));
-	}
+    public BmqModel modifierBmq(Bmq bmq) {
+        return new BmqModel(bmqService.modifierBmq(bmq));
+    }
 
-	public BmqModel obtenirBmq(Long bmqId) {
-		return new BmqModel(bmqService.obtenirBmq(bmqId));
-	}
+    public BmqModel obtenirBmq(Long bmqId) {
+        return new BmqModel(bmqService.obtenirBmq(bmqId));
+    }
 
-	public BmqModel obtenirBmq(String codeBmq) {
-		return new BmqModel(bmqService.obtenirBmq(codeBmq));
-	}
+    public BmqModel obtenirBmq(String codeBmq) {
+        return new BmqModel(bmqService.obtenirBmq(codeBmq));
+    }
 
-	public void supprimerBmq(Long bmqId) {
-		bmqService.supprimerBmq(bmqId);
-	}
+    public void supprimerBmq(Long bmqId) {
+        bmqService.supprimerBmq(bmqId);
+    }
 
-	public Page<BmqModel> listerBmq(Pageable p) {
-		List<BmqModel> bmqs = new ArrayList<>();
-		bmqService.listerBmq(p).getContent().forEach(b -> {
-			bmqs.add(new BmqModel(b));
-		});
-		return new PageImpl<>(bmqs);
-	}
+    public Page<BmqModel> listerBmq(Pageable p) {
+        final List<BmqModel> bmqs = new ArrayList<>();
+        bmqService.listerBmq(p).getContent().forEach(b -> {
+            bmqs.add(new BmqModel(b));
+        });
+        return new PageImpl<>(bmqs);
+    }
 
-	public Page<BmqModel> listerBmq(Long vendeurId, Pageable p) {
-		List<BmqModel> bmqs = new ArrayList<>();
-		bmqService.listerBmq(vendeurId, p).getContent().forEach(b -> {
-			bmqs.add(new BmqModel(b));
-		});
-		return new PageImpl<>(bmqs);
-	}
+    public Page<BmqModel> listerBmq(Long vendeurId, Pageable p) {
+        final List<BmqModel> bmqs = bmqService.listerBmq(vendeurId, p).getContent().stream().map(b -> {
+            return new BmqModel(b);
+        }).collect(Collectors.toList());
+        return new PageImpl<>(bmqs);
+    }
 
-	public Page<BmqModel> listerBmq(Date debut, Date fin, Pageable p) {
-		List<BmqModel> bmqs = new ArrayList<>();
-		bmqService.listerBmq(debut, fin, p).getContent().forEach(b -> {
-			bmqs.add(new BmqModel(b));
-		});
-		return new PageImpl<>(bmqs);
-	}
+    public Page<BmqModel> listerBmq(Date debut, Date fin, Pageable p) {
+        final List<BmqModel> bmqs = bmqService.listerBmq(debut, fin, p).getContent().stream().map(b -> {
+            return new BmqModel(b);
+        }).collect(Collectors.toList());
+        return new PageImpl<>(bmqs);
+    }
 
-	public Page<BmqModel> listerBmq(Long vendeurId, Date debut, Date fin, Pageable p) {
-		List<BmqModel> bmqs = new ArrayList<>();
-		bmqService.listerBmq(vendeurId, p).getContent().forEach(b -> {
-			bmqs.add(new BmqModel(b));
-		});
-		return new PageImpl<>(bmqs);
-	}
+    public Page<BmqModel> listerBmq(Long vendeurId, Date debut, Date fin, Pageable p) {
+        final List<BmqModel> bmqs = bmqService.listerBmq(vendeurId, p).getContent().stream().map(b -> {
+            return new BmqModel(b);
+        }).collect(Collectors.toList());
+        return new PageImpl<>(bmqs);
+    }
 
-	public Collection<LigneBmqModel> ajouterLigneBmq(Collection<LigneBmq> ligneBmqs) {
-		Collection<LigneBmqModel> lbs = new HashSet<>();
-		bmqService.ajouterLigneBmq(ligneBmqs).stream().forEach(lb -> {
-			lbs.add(new LigneBmqModel(lb));
-		});
-		return lbs;
-	}
+    public Collection<LigneBmqModel> ajouterLigneBmq(Collection<LigneBmq> ligneBmqs) {
+        return bmqService.ajouterLigneBmq(ligneBmqs).stream().map(lb -> {
+            return new LigneBmqModel(lb);
+        }).collect(Collectors.toList());
+    }
 
-	public LigneBmqModel obtenirLigneBmq(Long ligneBmqId) {
-		return new LigneBmqModel(bmqService.obtenirLigneBmq(ligneBmqId));
-	}
+    public LigneBmqModel obtenirLigneBmq(Long ligneBmqId) {
+        return new LigneBmqModel(bmqService.obtenirLigneBmq(ligneBmqId));
+    }
 
-	public void supprimerLigneBmq(Long ligneBmqId) {
-		bmqService.supprimerLigneBmq(ligneBmqId);
-	}
+    public void supprimerLigneBmq(Long ligneBmqId) {
+        bmqService.supprimerLigneBmq(ligneBmqId);
+    }
 
-	public BmqModel genererBmq(Long bmqId, Collection<Document> documents, Collection<Recouvrement> recouvrements) {
-		return new BmqModel(bmqService.genererBmq(bmqId, documents, recouvrements));
-	}
+    public BmqModel genererBmq(Long bmqId, Collection<Document> documents, Collection<Recouvrement> recouvrements) {
+        return new BmqModel(bmqService.genererBmq(bmqId, documents, recouvrements));
+    }
 
-	public RecouvrementModel ajouterRecouvrement(Recouvrement recouvrement) {
-		return new RecouvrementModel(bmqService.ajouterRecouvrement(recouvrement));
-	}
+    public RecouvrementModel ajouterRecouvrement(Recouvrement recouvrement) {
+        return new RecouvrementModel(bmqService.ajouterRecouvrement(recouvrement));
+    }
 
-	public RecouvrementModel modifierRecouvrement(Recouvrement recouvrement) {
-		return new RecouvrementModel(bmqService.modifierRecouvrement(recouvrement));
-	}
+    public RecouvrementModel modifierRecouvrement(Recouvrement recouvrement) {
+        return new RecouvrementModel(bmqService.modifierRecouvrement(recouvrement));
+    }
 
-	public RecouvrementModel obtenirRecouvrement(Long recouvrementId) {
-		return new RecouvrementModel(bmqService.obtenirRecouvrement(recouvrementId));
-	}
+    public RecouvrementModel obtenirRecouvrement(Long recouvrementId) {
+        return new RecouvrementModel(bmqService.obtenirRecouvrement(recouvrementId));
+    }
 
-	public void supprimerRecouvrement(Long recouvrementId) {
-		bmqService.supprimerRecouvrement(recouvrementId);
-	}
+    public void supprimerRecouvrement(Long recouvrementId) {
+        bmqService.supprimerRecouvrement(recouvrementId);
+    }
 
-	public void supprimerLigneBmq(Bmq bmq) {
-		bmqService.supprimerLigneBmq(bmq);
-	}
+    public void supprimerLigneBmq(Bmq bmq) {
+        bmqService.supprimerLigneBmq(bmq);
+    }
 
-	public LigneBmqTaxeModel ajouterLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
-		return new LigneBmqTaxeModel(bmqService.ajouterLigneBmqTaxe(ligneBmqTaxe));
-	}
+    public LigneBmqTaxeModel ajouterLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
+        return new LigneBmqTaxeModel(bmqService.ajouterLigneBmqTaxe(ligneBmqTaxe));
+    }
 
-	public LigneBmqTaxeModel modifierLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
-		return new LigneBmqTaxeModel(bmqService.modifierLigneBmqTaxe(ligneBmqTaxe));
-	}
+    public LigneBmqTaxeModel modifierLigneBmqTaxe(LigneBmqTaxe ligneBmqTaxe) {
+        return new LigneBmqTaxeModel(bmqService.modifierLigneBmqTaxe(ligneBmqTaxe));
+    }
 
-	public LigneBmqTaxeModel obtenirLigneBmqTaxe(Long ligneBmqTaxeId) {
-		return new LigneBmqTaxeModel(bmqService.trouverLigneBmqTaxe(ligneBmqTaxeId));
-	}
+    public LigneBmqTaxeModel obtenirLigneBmqTaxe(Long ligneBmqTaxeId) {
+        return new LigneBmqTaxeModel(bmqService.trouverLigneBmqTaxe(ligneBmqTaxeId));
+    }
 
-	public void supprimerLigneBmqTaxe(Long ligneBmqTaxeId) {
-		bmqService.supprimerLigneBmqTaxe(ligneBmqTaxeId);
-	}
+    public void supprimerLigneBmqTaxe(Long ligneBmqTaxeId) {
+        bmqService.supprimerLigneBmqTaxe(ligneBmqTaxeId);
+    }
 
-	public double chiffreAffaireHorsTaxe(Bmq bmq) {
-		return bmqService.chiffreAffaireHorsTaxe(bmq);
-	}
+    public double chiffreAffaireHorsTaxe(Bmq bmq) {
+        return bmqService.chiffreAffaireHorsTaxe(bmq);
+    }
 
-	public double chiffreAffaireTTC(Bmq bmq) {
-		return bmqService.chiffreAffaireTTC(bmq);
-	}
+    public double chiffreAffaireTTC(Bmq bmq) {
+        return bmqService.chiffreAffaireTTC(bmq);
+    }
 
-	public double valeurTotaleTaxe(Bmq bmq) {
-		return bmqService.valeurTotaleTaxe(bmq);
-	}
+    public double valeurTotaleTaxe(Bmq bmq) {
+        return bmqService.valeurTotaleTaxe(bmq);
+    }
 
-	public double valeurTaxe(Taxe taxe, Bmq bmq) {
-		return bmqService.valeurTaxe(taxe, bmq);
-	}
+    public double valeurTaxe(Taxe taxe, Bmq bmq) {
+        return bmqService.valeurTaxe(taxe, bmq);
+    }
 
-	public double venteComptant(Bmq bmq) {
-		return bmqService.venteComptant(bmq);
-	}
+    public double venteComptant(Bmq bmq) {
+        return bmqService.venteComptant(bmq);
+    }
 
-	public double valeurMarge(Bmq bmq) {
-		return bmqService.valeurMarge(bmq);
-	}
+    public double valeurMarge(Bmq bmq) {
+        return bmqService.valeurMarge(bmq);
+    }
 
-	public void genererVenteComptant(Bmq bmq) {
-		bmqService.genererVenteComptant(bmq);
-	}
+    public void genererVenteComptant(Bmq bmq) {
+        bmqService.genererVenteComptant(bmq);
+    }
 }
--- src/main/java/com/camlait/global/erp/service/produit/ProduitServiceDelegate.java
+++ src/main/java/com/camlait/global/erp/service/produit/ProduitServiceDelegate.java
@@ -1,9 +1,8 @@
 package com.camlait.global.erp.service.produit;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,100 +16,81 @@ import com.camlait.global.erp.domain.model.json.produit.ProduitModel;
 import com.camlait.global.erp.domain.model.json.produit.ProduitTaxeModel;
 import com.camlait.global.erp.domain.produit.CategorieProduit;
 
-
 public class ProduitServiceDelegate {
-    
+
     @Autowired
     private IProduitService produitService;
-    
+
     public ProduitModel obtenirProduit(Long produitId) {
         return new ProduitModel(produitService.obtenirProduit(produitId));
     }
-    
+
     public ProduitModel obtenirProduit(String codeProduit) {
         return new ProduitModel(produitService.obtenirProduit(codeProduit));
     }
-    
+
     public Collection<ProduitModel> listerProduit(CategorieProduit categorie) {
-        final Collection<ProduitModel> pms = new HashSet<>();
-        produitService.listerProduit(categorie).stream().forEach(p -> {
-        	//System.out.println(p);
-            //pms.add(new ProduitModel(p));
-            pms.add(ProduitModel.createProduitModel(p));
-        });
-        return pms;
+        return produitService.listerProduit(categorie).stream().map(p -> {
+            return ProduitModel.createProduitModel(p);
+        }).collect(Collectors.toList());
     }
-    
+
     public Page<ProduitModel> listerProduit(Pageable p) {
-        final List<ProduitModel> pms = new ArrayList<>();
-        produitService.listerProduit(p).getContent().stream().forEach(pr -> {
-            pms.add(new ProduitModel(pr));
-        });
+        final List<ProduitModel> pms = produitService.listerProduit(p).getContent().stream().map(pr -> {
+            return new ProduitModel(pr);
+        }).collect(Collectors.toList());
         return new PageImpl<ProduitModel>(pms, p, pms.size());
     }
-    
+
     public Collection<ProduitModel> listerProduit(Collection<CategorieProduit> categories) {
-        final Collection<ProduitModel> pms = new HashSet<>();
-        produitService.listerProduit(categories).stream().forEach(p -> {
-            pms.add(new ProduitModel(p));
-        });
-        return pms;
+        return produitService.listerProduit(categories).stream().map(p -> {
+            return new ProduitModel(p);
+        }).collect(Collectors.toList());
     }
-    
+
     public CategorieProduitModel obtenirCategorieProduit(Long categorieId) {
         return new CategorieProduitModel(produitService.obtenirCategorieProduit(categorieId));
     }
-    
+
     public CategorieProduitModel obtenirCategorieProduit(String codeCategorie) {
         return new CategorieProduitModel(produitService.obtenirCategorieProduit(codeCategorie));
     }
-    
+
     public Collection<CategorieProduitModel> listerCategorieProduit() {
-        Collection<CategorieProduitModel> cms = new HashSet<>();
-        produitService.listerCategorieProduit().stream().forEach(c -> {
-            cms.add(new CategorieProduitModel(c));
-        });
-        return cms;
+        return produitService.listerCategorieProduit().stream().map(c -> {
+            return new CategorieProduitModel(c);
+        }).collect(Collectors.toList());
     }
-    
+
     public Collection<CategorieProduitModel> listerCategorie(Long parentId) {
-        Collection<CategorieProduitModel> cms = new HashSet<>();
-        produitService.listerCategorie(parentId).stream().forEach(c -> {
-            cms.add(new CategorieProduitModel(c));
-        });
-        return cms;
+        return produitService.listerCategorie(parentId).stream().map(c -> {
+            return new CategorieProduitModel(c);
+        }).collect(Collectors.toList());
     }
-    
+
     public Collection<CategorieProduitModel> listerCategorieProduit(String motCle) {
-        Collection<CategorieProduitModel> cms = new HashSet<>();
-        produitService.listerCategorieProduit(motCle).stream().forEach(c -> {
-            cms.add(new CategorieProduitModel(c));
-        });
-        return cms;
-        
+        return produitService.listerCategorieProduit(motCle).stream().map(c -> {
+            return new CategorieProduitModel(c);
+        }).collect(Collectors.toList());
     }
-    
+
     public Collection<ProduitModel> listerProduit(String motCle) {
-        final Collection<ProduitModel> pms = new HashSet<>();
-        produitService.listerProduit(motCle).stream().forEach(p -> {
-            pms.add(new ProduitModel(p));
-        });
-        return pms;
+        return produitService.listerProduit(motCle).stream().map(p -> {
+            return new ProduitModel(p);
+        }).collect(Collectors.toList());
     }
-    
+
     public ProduitTaxeModel obtenirProduitTaxe(Long produitTaxeId) {
         return new ProduitTaxeModel(produitService.obtenirProduitTaxe(produitTaxeId));
     }
-    
+
     public CategorieProduitTaxeModel obtenirCategorieProduitTaxe(Long categorieProduitTaxeId) {
         return new CategorieProduitTaxeModel(produitService.obtenirCategorieProduitTaxe(categorieProduitTaxeId));
     }
-    
+
     public Collection<CategorieProduitModel> listerCategorie(Portee portee) {
-        Collection<CategorieProduitModel> cms = new HashSet<>();
-        produitService.listerCategorie(portee).stream().forEach(c -> {
-            cms.add(new CategorieProduitModel(c));
-        });
-        return cms;
+        return produitService.listerCategorie(portee).stream().map(c -> {
+            return new CategorieProduitModel(c);
+        }).collect(Collectors.toList());
     }
 }
